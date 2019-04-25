e117d7015fd96e0795745e871c6d14bf130af093
oliveirajade <oliveiradepaulajaqueline@gmail.com>
2019-04-24 22:22:52 -0300

    Criação do Jogador

---
+++ Arkanoid/Assets/Scripts/Bola.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    public Vector2 velocidade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(velocidade);
+    }
+}
